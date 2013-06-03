4e45aa3346a3e5f9300655600189b21b5b06852a
Hemant V. Torsekar <hvt@vectd.com>
2013-06-02 21:27:24 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidstr::Application.config.secret_token = '545c003d5586d8fe964686ae4621c383ac458358899c8522a987fb5bfd9ee0d405afb417ebef7dd1a78b2d5cae5b05a3cbfed96057d0ad65239c46bbf80bf1e6'
